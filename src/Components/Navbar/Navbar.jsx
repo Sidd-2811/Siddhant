99a98fb34dc609bd4eb31d7c8128b2ac6a214329
Sidd-2811 <siddhantmourya2811.com>
2024-07-08 13:43:52 +0530

    addpages and components

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
