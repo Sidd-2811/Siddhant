d7c04b8fb2a2f5f621a00f055ef69aac5400089d
Sidd-2811 <siddhantmourya2811.com>
2024-07-08 14:53:19 +0530

    adding navbar css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // import React from 'react'
 
+import Navbar from "./Components/Navbar/Navbar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
 import "./Navbar.css"
+import menu from "../../assets/menu.png"
+import logo from "../../assets/logo.png"
+import search from "../../assets/search.png"
+import upload from "../../assets/upload.png"
+import more from "../../assets/more.png"
+import notification from "../../assets/notification.png"
+import profile from "../../assets/jack.png"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+      <img className="menu" src={menu} alt="" />
+      <img className="logo" src={logo} alt="" />
+      </div>
+
+      <div className="nav-middle flex-div">
+         <div className="search-box flex-div">
+          <input type="text" placeholder="search" />
+          <img src={search} alt="" />
+         </div>
+      </div>
+
+      <div className="nav-right flex-div">
+      <img src={upload} alt="" />
+      <img src={more} alt="" />
+      <img src={notification} alt="" />
+      <img src={profile}  className="user-icon" alt="" />
+      </div>
+    </nav >
   )
 }
 
